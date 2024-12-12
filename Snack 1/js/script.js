8cc56352f8c9a7921a86d2e33297238839c6c2fc
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-12 15:57:57 +0100

    feat: init snack 1

---
+++ Snack 1/js/script.js
@@ -0,0 +1,26 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const bikesType = [
+    {
+        type: "Specialized",
+        peso: 2000
+    },
+
+    {
+        type: "Kona",
+        peso: 2800
+    },
+
+    {
+        type: "Scott",
+        peso: 3200
+    },
+
+    {
+        type: "Santa Cruz",
+        peso: 1900
+    }
+]
